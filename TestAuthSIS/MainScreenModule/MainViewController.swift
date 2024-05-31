8a3c05e105a96e8358c1cd053e2cf6b4fce7611a
MihaelNevash <mihaelnevash@gmail.com>
2024-05-31 19:38:37 +0300

    initial

--- TestAuthSIS/MainScreenModule/MainViewController.swift
+++ TestAuthSIS/MainScreenModule/MainViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    //MARK: Properties
+    
+    //MARK: SubViews
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
